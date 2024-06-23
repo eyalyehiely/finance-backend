14c0cc1453f8f303c70bc502ebda658db4d67f70
eyalyehiely <eyalwork0@gmail.com>
2024-06-23 17:53:55 +0300

    sign up validations

--- users/serializers.py
+++ users/serializers.py
@@ -3,18 +3,29 @@ from rest_framework.authtoken.admin import User
 from .models import CustomUser
 from .family_models import Family
 
-from rest_framework import serializers
-from .models import CustomUser
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     age = serializers.SerializerMethodField()
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'gender', 'life_status','num_of_children', 'family', 'phone_number', 'birth_date','age', 'profession', 'address', 'updated_at']
+        fields = ['id', 'email', 'first_name', 'last_name', 'gender', 'life_status',
+                  'num_of_children', 'family', 'phone_number', 'birth_date', 'age',
+                  'profession', 'address', 'updated_at']
+        read_only_fields = ['id', 'updated_at']  # Make id and updated_at read-only
 
-    def get_age(self,obj):
+    def get_age(self, obj):
         return obj.age
-    
+
+    def validate_email(self, value):
+        # Check if email already exists
+        if self.instance and self.instance.email == value:
+            return value  # If updating, email can remain unchanged
+
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email already exists.')
+        return value
 
 
 class FamilySerializer(serializers.ModelSerializer):
