696eab3f47e09fe063ab178c79d05e8f808ce9aa
eyalyehiely <eyalwork0@gmail.com>
2024-06-27 00:18:57 +0300

    improve expenses dashboard card

--- finance/settings.py
+++ finance/settings.py
@@ -171,14 +171,11 @@ WSGI_APPLICATION = 'finance.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'YxwYKJCygbZeNZxuvcljzHGKodIkHWmd',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '41684',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
--- frontend/src/partials/dashboard/ExpensesCard.jsx
+++ frontend/src/partials/dashboard/ExpensesCard.jsx
@@ -7,13 +7,15 @@ function ExpensesCard() {
     const [amount, setAmount] = useState(null);
 
     useEffect(() => {
-        fetchCurrentMonthExpenses(token, setAmount); // Call fetchData when the component mounts
+        if (token) {
+            fetchCurrentMonthExpenses(token, setAmount); // Call fetchData when the component mounts
+        }
     }, [token]); // Include token as a dependency to useEffect
 
     // Conditional rendering based on the value of amount
     return (
         <>
-            {amount > 0 ? (
+            {amount !== null ? (
                 <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
                     <div className="px-5 pt-5">
                         <header className="flex justify-between items-start mb-2">
@@ -23,13 +25,13 @@ function ExpensesCard() {
                         <h2 dir="rtl" className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">הוצאות (החודש)</h2>
                         <div dir="rtl" className="flex items-start">
                             <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{amount}₪</div>
+                            {/* Example placeholder for percentage */}
                             <div className="text-sm font-semibold text-white px-1.5 bg-amber-500 rounded-full">-14%</div>
                         </div>
                     </div>
-                    
                 </div>
             ) : (
-                <span>אין הוצאות</span>
+                <span>טוען...</span> // Placeholder for loading state
             )}
         </>
     );
