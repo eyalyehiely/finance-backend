31399a63aabd7eb912595da8ffd13d15337dad2e
eyalyehiely <139653070+eyalyehiely@users.noreply.github.com>
2024-06-28 16:48:10 +0300

    Delete frontend directory

--- frontend/src/components/AddComma.jsx
+++
@@ -1,9 +0,0 @@
-function addCommaToNumber(number) {
-  if (number === null || number === undefined) {
-      return '';
-  }
-
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-export default addCommaToNumber;
