6eac3777ca288bb98a402d11bef22776cecc27ff
eyalyehiely <139653070+eyalyehiely@users.noreply.github.com>
2024-06-28 16:49:31 +0300

    Delete frontend directory

--- frontend/src/components/AddComma.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-function AddCommaToNumber(number) {
-    return (
-        number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    )
-  }
-
-export  default AddCommaToNumber
