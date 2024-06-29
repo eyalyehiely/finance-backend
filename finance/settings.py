9c8e3d9359496ea44ce4d2622c78ce6a07c649c5
eyalyehiely <eyalwork0@gmail.com>
2024-06-29 14:04:11 +0300

    eviroment settings

--- finance/settings.py
+++ finance/settings.py
@@ -161,11 +161,11 @@ WSGI_APPLICATION = 'finance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ('DATABASE_NAME'),
-        'USER': os.environ('DATABASE_USER'),
-        'PASSWORD': os.environ('DATABASE_PASSWORD'),
-        'HOST': os.environ('DATABASE_HOST'),
-        'PORT': os.environ('DATABASE_PORT'),                   
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),                   
     }
 }
 # DATABASES = {
