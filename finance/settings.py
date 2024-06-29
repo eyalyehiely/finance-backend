a13189aa55d7715112fc6c822e09154cdbcaefe8
eyalyehiely <eyalwork0@gmail.com>
2024-06-29 16:37:37 +0300

    defined CORS_ALLOWED_ORIGINS

--- finance/settings.py
+++ finance/settings.py
@@ -123,7 +123,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'finance.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get('FRONTEND_URL'),
+    os.environ.get('FRONTEND_URL','http://localhost:5173'),
     # Add other origins as needed
 ]
 CORS_ORIGIN_ALLOW_ALL = True
