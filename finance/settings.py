f08b557b1b99076fce479f85cf4eb4915cabedb7
eyalyehiely <eyalwork0@gmail.com>
2024-07-04 14:20:16 +0300

    remove social

--- finance/settings.py
+++ finance/settings.py
@@ -152,8 +152,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'finance.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
 DATABASES = {
@@ -209,6 +207,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+#mailing
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -216,10 +215,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'eyalwork0@gmail.com'
 EMAIL_HOST_PASSWORD = 'kanw zgwa xeot cxfx'
 DEFAULT_FROM_EMAIL = 'eyalwork0@gmail.com'
-
-
-
-
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 
